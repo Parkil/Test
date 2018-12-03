922b9dcecac63c92d5b2dfbffb78e84babac5c90
Parkil <Parkil@DESKTOP-QJTE9IN>
2018-12-03 14:00:48 +0900

    jsch scp 다운로드 예제 업로드

---
+++ src/test/jsch/JSchDownloadExec.java
@@ -0,0 +1,155 @@
+package test.jsch;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+/*
+ * jsch 라이브러리를 이용하여 sftp download를 수행하는 예제 
+ * 해당예제는 exec모드를 이용하여 scp 커맨드를 실행하는 방식으로  다운로드를 수행
+ */
+public class JSchDownloadExec {
+	static int checkAck(InputStream in) throws IOException {
+		int b = in.read();
+		// b may be 0 for success,
+		// 1 for error,
+		// 2 for fatal error,
+		// -1
+		if (b == 0)
+			return b;
+		if (b == -1)
+			return b;
+
+		if (b == 1 || b == 2) {
+			StringBuffer sb = new StringBuffer();
+			int c;
+			do {
+				c = in.read();
+				sb.append((char) c);
+			} while (c != '\n');
+			if (b == 1) { // error
+				System.out.print(sb.toString());
+			}
+			if (b == 2) { // fatal error
+				System.out.print(sb.toString());
+			}
+		}
+		return b;
+	}
+
+	public static void main(String[] arg) {
+		String srcUri = "/data/uni_ftp/new/00602567884484/00602567884484_20181026070427900/UMG_Metadata_mnetgsc_00602567884484_2018-10-26_06-17-43.xml";
+		String destUri = "d:/download/UMG_Metadata_mnetgsc_00602567884484_2018-10-26_06-17-43.xml";
+		
+		
+		FileOutputStream fos = null;
+		try {
+			JSch jsch = new JSch();
+			Session session = jsch.getSession("uni_ftp", "210.122.111.15", 22);
+			session.setConfig("StrictHostKeyChecking", "no");
+			session.setPassword("!@12uni_ftp");
+			
+			session.connect();
+
+			String command = "scp -f " + srcUri;
+			Channel channel = session.openChannel("exec");
+			((ChannelExec) channel).setCommand(command);
+
+			// get I/O streams for remote scp
+			OutputStream out = channel.getOutputStream();
+			InputStream in = channel.getInputStream();
+
+			channel.connect();
+
+			byte[] buf = new byte[1024];
+
+			// send '\0'
+			buf[0] = 0;
+			out.write(buf, 0, 1);
+			out.flush();
+
+			while (true) {
+				int c = checkAck(in);
+				if (c != 'C') {
+					break;
+				}
+
+				// read '0644 '
+				in.read(buf, 0, 5);
+
+				long filesize = 0L;
+				while (true) {
+					if (in.read(buf, 0, 1) < 0) {
+						// error
+						break;
+					}
+					if (buf[0] == ' ')
+						break;
+					filesize = filesize * 10L + (long) (buf[0] - '0');
+				}
+
+				String file = null;
+				for (int i = 0;; i++) {
+					in.read(buf, i, 1);
+					if (buf[i] == (byte) 0x0a) {
+						file = new String(buf, 0, i);
+						break;
+					}
+				}
+
+				// send '\0'
+				buf[0] = 0;
+				out.write(buf, 0, 1);
+				out.flush();
+
+				// read a content of lfile
+				fos = new FileOutputStream(destUri);
+				int foo;
+				while (true) {
+					if (buf.length < filesize)
+						foo = buf.length;
+					else
+						foo = (int) filesize;
+					foo = in.read(buf, 0, foo);
+					if (foo < 0) {
+						// error
+						break;
+					}
+					fos.write(buf, 0, foo);
+					filesize -= foo;
+					if (filesize == 0L)
+						break;
+				}
+				fos.close();
+				fos = null;
+
+				if (checkAck(in) != 0) {
+					System.exit(0);
+				}
+
+				// send '\0'
+				buf[0] = 0;
+				out.write(buf, 0, 1);
+				out.flush();
+			}
+
+			session.disconnect();
+
+			System.exit(0);
+		} catch (Exception e) {
+			System.out.println(e);
+			try {
+				if (fos != null)
+					fos.close();
+			} catch (Exception ee) {
+			}
+		}
+	}
+}
